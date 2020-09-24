05b66ef425ab30d5e838ab6b41375e5ca22a3cda
Hubbotu <34053496+Hubbotu@users.noreply.github.com>
2020-09-24 07:55:00 +0300

    Add files via upload

---
+++ ruRU.lua
@@ -0,0 +1,13 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("MythicPlusLoot", "ruRU", true)
+
+L["MythicPlusLoot: Loaded"] = "MythicPlusLoot: Загружен"
+L["MythicPlusLoot: Enabled"] = "MythicPlusLoot: Включен"
+L["MythicPlusLoot: Disabled"] = "MythicPlusLoot: Отключен"
+L["MythicPlusLoot does not have any function command."] = "MythicPlusLoot не имеет функциональной команды."
+
+L["This shows the level of the item you'll find in this week's chest."] = "Показывает уровень предмета, который вы найдете в сундуке на этой неделе."
+L["Weekly Chest Reward"] = "Еженедельная награда за сундук"
+
+L["Loot Item Level: "] = "Уровень предмета добычи: "
+L["Weekly Chest Item Level: "] = "Еженедельный сундук Уровень предмета: "
+L["Weekly Residuum Amount: "] = "Еженедельное количество Пыли титанов: "
